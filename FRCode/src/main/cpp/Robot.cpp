3de6fcf84a127a21e10c8cbc1b852bb60d4b6541
Siddhant Hullur <siddhanthullur@gmail.com>
2021-08-14 13:05:01 -0700

    initial commit

---
+++ FRCode/src/main/cpp/Robot.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Robot.h"
+
+void Robot::RobotInit() {
+  joystick = new frc::Joystick(0);
+  motor = new rev::CANSparkMax(0, rev::CANSparkMaxLowLevel::MotorType::kBrushless)
+}
+void Robot::RobotPeriodic() {}
+
+void Robot::AutonomousInit() {}
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {}
+void Robot::TeleopPeriodic() {
+  motor->Set(joystick->GetY());
+}
+
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
+
